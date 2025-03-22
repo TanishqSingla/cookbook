93873306fb0604f6f19ffc3ed877d6600c9bd309
TanishqSingla <tanishqsingla00@gmail.com>
2025-03-22 13:03:42 +0530

    chore: config changes

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/dev-cookbook/', // Replace with your repository name
+  base: '/cookbook/', // Replace with your repository name
 }) 
